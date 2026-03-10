e3bc24039acec2d2e2c3484e47e01b6e9da5da79
Kim Nguyen <3666419+isallcaps@users.noreply.github.com>
2026-03-09 21:54:11 -0400

    Attempting to fix the base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://isallcaps.github.io',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
